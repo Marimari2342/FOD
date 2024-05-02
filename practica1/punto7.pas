a3352515e948ec7ae4d3775cc926e970974d1478
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-01 21:30:07 -0300

    commit punto 7a)

--- practica1/punto7.pas
+++ practica1/punto7.pas
@@ -1,9 +1,3 @@
-{Crear un archivo binario a partir de la información almacenada en un archivo de texto. 
-El nombre del archivo de texto es: “novelas.txt”. La información en el archivo de texto
-consiste en: código de novela, nombre, género y precio de diferentes novelas argentinas.
-Los datos de cada novela se almacenan en dos líneas en el archivo de texto. La primera
-línea contendrá la siguiente información: código novela, precio y género, y la 
-segunda línea almacenará el nombre de la novela.}
 Program punto7;
 type
     novelaR = record
@@ -30,8 +24,23 @@ var
     txt: Text;
     n:novelaR;
 begin
-    
+    {asignar nombre a archivo binario y abrir}
+    asignarNombre(arc_nov);
+    Rewrite(arc_nov);
+    {asignar a txt = “novelas.txt” y abrir archivo de texto}
+    Assign(txt,'novelas.txt');
+    Reset(txt);
+    {mientras no se termine el archivo de texto, cargar el archivo binario} 
+    while (not Eof(txt)) do begin
+        readLn(txt,n.cod,n.precio,n.genero);
+        readLn(txt,n.nombre);
+        write(arc_nov,n);
+    end;
+    {cerrar archivos}
+    Close(arc_nov);
+    Close()
 end;
+
 {programa principal}
 var
     arc_nov = novelas;
