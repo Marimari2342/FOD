fd0fea99e6764dfc94bd9ea69a8c413dd6b0b2c6
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-05 11:26:09 -0300

    commit punto1 fin

--- practica2/punto1.pas
+++ practica2/punto1.pas
@@ -5,11 +5,12 @@ type
         nombre:string[15];
         monto:real;
     end;
-    comisiones = file of empR;
+
 procedure Compactar();
 var
     comisiones:Text;
     arc_comp:Text;
+    eLei,eAcum:empR;
 begin
     {asignar a variable y abrir archivo que voy a leer}
     Assign(comisiones,'comisiones.txt');
@@ -17,8 +18,16 @@ begin
     {asignar a variable y crear archivo donde voy a guardar}
     Assign(arc_comp,'compactado.txt');
     Rewrite(arc_comp);
-    
-
+    Read(comisiones,eLei.cod,eLei.nombre,eLei.monto);
+    while (not Eof(comisiones)) do begin
+      eAcum.cod := eLei.cod;
+      eAcum.monto := 0;
+      while (not Eof(comisiones))and(eLei.cod = eAcum.cod) do begin
+        eAcum.monto+=eLei.monto;
+        Read(comisiones,eLei.cod,eLei.nombre,eLei.monto);
+      end;
+      WriteLn(arc_comp,eAcum.cod,' ',eAcum.monto:0:2,' ',eAcum.nombre);
+    end;
     {cerrar archivos}
     Close(arc_comp);
     Close(comisiones);
