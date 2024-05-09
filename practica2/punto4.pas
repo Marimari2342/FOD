838ff6d6178a869d09fa0d9407c245c59a743bf5
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-09 12:57:52 -0300

    commit en proceso p4

--- practica2/punto4.pas
+++ practica2/punto4.pas
@@ -1,6 +1,6 @@
 Program punto4;
 const
-    valorAlto = 99999;
+    valorAlto = 'ZZZZ';
 type
     provinciaR = record
         nombre:string;
@@ -18,8 +18,46 @@ type
 
 {codigo}
 
-{programa principal}
+{cargar archivo detalle 1 desde un txt}
+
+{cargar archivo detalle 2 desde un txt}
+
+procedure Leer(var detalle:agencia;var a: agenciaR);
 var
 begin
-  
+    if(not Eof(detalle))then
+      Read(detalle,a)
+    else
+      a.nombreProv:=valorAlto;
+end;
+
+{actualizar archivo maestro desde los dos archivos detalle}
+procedure ActualizarMaestro(var maestro:provincia;var detalle1,detalle2: agencia);
+var
+    a1,a2=agenciaR;
+begin
+    {abrir archivos}
+    Reset(maestro);
+    Reset(detalle1);
+    Reset(detalle2);
+    Leer(detalle1,a1);
+    Leer(detalle2,a2);
+
+    {cerrar archivos}
+    Close(maestro);
+    Close(detalle1);
+    Close(detalle2);
+end;
+
+{programa principal}
+var
+    maestro:provincia;
+    detalle1,detalle2:agencia;
+begin
+    Assign(maestro,'archivomaestro');
+    Assign(detalle1,'archivodet1');
+    Assign(detalle2,'archivodet2');
+    CargarDet1(detalle1);
+    CargarDet2(detalle2);
+    ActualizarMaestro(maestro,detalle1,detalle2);
 end.
