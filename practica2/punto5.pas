4fc53d4339b8a75551959a4a7a13b9e721c8c97d
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-13 23:02:35 -0300

    commit

---
+++ practica2/punto5.pas
@@ -0,0 +1,18 @@
+Program punto5;
+const
+    valorAlto = 99999;
+type
+    productoR = record
+        cod:Integer;
+        nombre:String[20];
+        desc:String[40];
+        stockDisp:integer;
+        stockMin:integer;
+        precio:real;
+    end;
+    {---en proceso---}
+{programa principal}
+var
+begin
+  
+end.
