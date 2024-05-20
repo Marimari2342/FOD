4c934fd6e15e65a64e911dd4768be80125b7d2e3
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-20 10:50:33 -0300

    commit punto b)

--- parciales/archivos/examen1.pas
+++ parciales/archivos/examen1.pas
@@ -3,16 +3,23 @@ const
     valorAlto = '9999';
     N = 30;
 type
+    subDia = 1..31;
+    subMes = 1..12;
+    fechR = record
+        dia:subDia;
+        mes:subMes;
+    end;
     ventaR = record
         cod_farmaco:string[4];
         nombre:string[20];
-        fecha:string[10];
+        fecha:fecR;
         cantidad_vendida:integer;
         forma_pago:string[7];
     end;
     detalle = file of ventaR;
     vecDet = array [1..N] of detalle;
     vecVent = array [1..N] of ventaR;
+    vecFech = array [1..31] of integer; 
 
 procedure leer (var d:detalle; var v:ventaR);
 begin
@@ -38,6 +45,7 @@ end;
 
 {Recibe los 30 archivos de ventas e informa por pantalla el fármaco con mayor 
 cantidad_vendida. --> corte de control}
+{Hago un corte de control farmaco por farmaco y me guardo el que tiene más ventas.}
 procedure punto_a (var d:vecDet);
 var
     i:integer;
@@ -66,7 +74,40 @@ begin
       Close(d[i]); {preguntar si necesito cerrar los detalles también}
 end;
 
-{codigo del b y c}
+{Recibe los 30 archivos de ventas e informa por pantalla la fecha en la que se produjeron
+más ventas al contado mostrando fecha y cantidad de pagos contado.}
+{Armo un vector contador en donde cargo los pagos al contado para cada fecha.
+Luego recorro el vector buscando el máximo.
+Supongo que no puedo hacer corte de control porque primeramente se ordena por codigo. ??}
+procedure punto_b (var d:vecDet);
+var
+    vF:vecFech;
+    i,maxCant,maxDia:integer;
+    min:ventaR;
+begin
+    {poner en cero el vector contador de ventas}
+    for i := 1 to subDia do
+        vF[i] := 0;
+    for i := 1 to N do begin
+      Reset(d[i]);
+      leer(d[i],v[i]);
+    end; 
+    minimo(d,v,min);
+    while (min.cod_farmaco<>valorAlto) do begin
+      if(min.forma_pago='contado') then
+        vF[min.fecha.subDia]+=1;
+      minimo(d,v,min);
+    end;
+    maxCant:=0;
+    for i := 1 to subDia do
+      if (vF[i]>maxCant) then begin
+        maxCant := vF[i];
+        maxDia := i;
+      end;
+    WriteLn('La fecha en la que se produjeron más ventas es: ',maxDia,' con ',maxCant,' ventas.');
+end;
+
+{codigo del c}
 
 {programa principal}
 var
