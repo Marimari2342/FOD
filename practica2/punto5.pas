837a8ca78029e3a328e005d4af73d3a126baa858
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-19 21:35:16 -0300

    commit en proceso

--- practica2/punto5.pas
+++ practica2/punto5.pas
@@ -1,10 +1,9 @@
 {actualizar archivo maestro con 30 detalles. --> N=30
 0, 1 o más registros por producto en archivo detalle --> valorAlto
-además... crear txt con información
-}
+además... crear txt con información}
 Program punto5;
 const
-    valorAlto = 99999;
+    valorAlto = 9999;
     N = 30;
 type
     productoR = record
@@ -23,7 +22,29 @@ type
     detalle = file of prod_sucR;
     vecDet = array [1..N] of detalle;
     vecProd = array [1..N] of prod_sucR;
-    {---en proceso---}
+    
+procedure leer(var d: detalle; var p:prod_sucR);
+begin
+  if(not eof(d)) then
+    read(d,p)
+  else 
+    p.cod:=valorAlto;
+end;
+
+procedure minimo(var d:vecDet; var p:vecProd; var min:prod_sucR);
+var
+    i,minInd:integer;
+begin
+    min.cod:=valorAlto;
+    for i := 1 to N do
+      if (d[i].cod <= min.cod) then begin
+        min := d[i];
+        minInd := i;
+      end;
+    read(d[minInd],p[minInd]);
+end;
+
+
 {programa principal}
 var
 begin
