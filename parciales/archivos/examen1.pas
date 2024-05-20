a56892f4af5f39c84f35f4356cc4092608bed58d
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-20 00:23:14 -0300

    commit examen1

--- parciales/archivos/examen1.pas
+++ parciales/archivos/examen1.pas
@@ -36,9 +36,44 @@ begin
     read(d[minInd],v[minInd]);
 end;
 
+{Recibe los 30 archivos de ventas e informa por pantalla el fármaco con mayor 
+cantidad_vendida. --> corte de control}
+procedure punto_a (var d:vecDet);
+var
+    i:integer;
+    v:vecVent;
+    min,farmMasVend,act:ventaR;
+    cantAct,codAct:integer;
+begin
+    for i := 1 to N do begin
+      Reset(d[i]);
+      leer(d[i],v[i]);
+    end; 
+    minimo(d,v,min);
+    farmMasVend.cantidad_vendida:=0;
+    while(min.cod_farmaco<>valorAlto)do begin
+      act.cantidad_vendida:=0;
+      act.cod_farmaco:=min.cod_farmaco;
+      while(codAct=min.cod_farmaco)do begin
+        cantAct+=min.cantidad_vendida;
+        minimo(d,v,min);
+      end;
+      if (act.cantidad_vendida>=farmMasVend.cantidad_vendida) then
+        farmMasVend:=act;
+    end;
+    WriteLn('El farmaco más vendido es el ',farmMasVend.nombre,' con ',farmMasVend.cantidad_vendida,' ventas');
+    for i := N downTo 1 do
+      Close(d[i]); {preguntar si necesito cerrar los detalles también}
+end;
+
+{codigo del b y c}
+
 {programa principal}
 var
-
+  i:integer;
+  d:vecDet;
 begin
-  
+  for i := 1 to N do
+    Assign(d[i],'detalle',i); {preguntar si se concatenaba así o como}
+    punto_a(d);
 end.
