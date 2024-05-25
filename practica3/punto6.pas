ffd67e9bc2a4d5ee3e6718c1594accb5d24bf1f3
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-25 00:15:33 -0300

    commit proceso

--- practica3/punto6.pas
+++ practica3/punto6.pas
@@ -8,17 +8,29 @@ type
         stock:integer;
         precio_unitario:real;
     end;
-    arc_prenda = file of prendaR;
-    arc_bajas = file of integer;
+    maestro = file of prendaR;
+    detalle = file of integer;
 
 {actualizar prendas con un archivo que indica cod_prenda de las prendas que 
 quedan obsoletas --> ELIMINAR
 baja logica --> modificar el stock de la prenda correspondiente a valor negativo}
-procedure baja_prenda (var arc_p:arc_prenda; var arc_b:arc_bajas);
+procedure baja_prenda (var m:maestro; var d:detalle);
+var
+begin
+end;
+
+{copiar las prendas que no están marcadas como borradas --> compactación del archivo 
+renombrar el archivo nuevo con el nombre del archivo maestro original}
+procedure compactar (var m,mN:maestro);
+var
+begin
+end;
 
 {programa principal}
 var
-
+    m,mN:maestro;
+    d:detalle;
 begin
-  
+    baja_prenda(m,d);
+    compactar(m,mN);
 end.
