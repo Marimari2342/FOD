9d7d49332d6b9ba7bc96d00a68a93bfa63da8b4e
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-25 00:02:32 -0300

    commit

--- practica3/punto6.pas
+++ practica3/punto6.pas
@@ -1,5 +1,14 @@
 program punto6;
-
+type
+    prendaR = record
+        cod_prenda:integer;
+        descripción:string[30];
+        colores:string[10];
+        tipo_prenda:string[10];
+        stock:integer;
+        precio_unitario:real;
+    end;
+     
 {programa principal}
 var
 
