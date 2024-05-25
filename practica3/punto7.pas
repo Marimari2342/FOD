1431af2c25677fa44a239af7f214ae3db537acbf
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-25 11:51:27 -0300

    commit arreg

--- practica3/punto7.pas
+++ practica3/punto7.pas
@@ -24,8 +24,10 @@ begin
         esta:=false;
         while ((not Eof(m))and(not esta)) do begin
             Read(m,ave);
-            if (m.cod=cod) then begin
-              m.nombre:='***'; {marca de ave eliminada}
+            if (ave.cod=cod) then begin
+              ave.nombre:='***'; {marca de ave eliminada}
+              Seek(m,FilePos(m)-1);
+              Write(m,ave);
               esta:=true;
             end;
         end;
@@ -42,7 +44,19 @@ registro del archivo en la posición del registro a borrar y luego elimina del a
 último registro para evitar registros duplicados}
 procedure compactar(var m:maestro);
 var
+    ave,auxA:aveR;
+    aux:integer;
 begin
+    Reset(m);
+    while (not Eof(m)) do begin
+      Read(m,ave);
+      if(ave.nombre='***')then begin{registro a eliminar}
+        aux:=FilePos(m)-1;  {apunto al registro marcado para eliminar}
+        Seek(m,(FileSize(m)));  {voy al final}
+        Read(m,auxA); {guardo el ultimo registro en auxA}
+        
+    end;
+    Close(m);
 end;
 
 {programa principal}
