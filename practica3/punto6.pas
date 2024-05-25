6715ab85a829ead63400e5b6857153950d7a8deb
Marimari2342 <marianroj@hotmail.com.ar>
2024-05-25 00:07:27 -0300

    commit planteando punto 6

--- practica3/punto6.pas
+++ practica3/punto6.pas
@@ -8,7 +8,14 @@ type
         stock:integer;
         precio_unitario:real;
     end;
-     
+    arc_prenda = file of prendaR;
+    arc_bajas = file of integer;
+
+{actualizar prendas con un archivo que indica cod_prenda de las prendas que 
+quedan obsoletas --> ELIMINAR
+baja logica --> modificar el stock de la prenda correspondiente a valor negativo}
+procedure baja_prenda (var arc_p:arc_prenda; var arc_b:arc_bajas);
+
 {programa principal}
 var
 
